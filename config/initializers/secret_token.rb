f1b54bfa062968c078c3339a1450eccbb99e0194
noahneighbors <noah.neighbors@gmail.com>
2013-07-20 20:06:07 -0700

    added rails v4.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battlearena::Application.config.secret_key_base = '0424c38ea08aa26ab59b03148d609bf78ad219e7bdb6984ea4998ca1ca4408f4e77215e7d54303939949cec9d93c24d3c4ba7dd8a7b33b8269789631d52d805a'
